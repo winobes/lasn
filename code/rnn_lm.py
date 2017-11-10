6dd3526abc2ab20efa651a60b434b5a705873204
Bill Noble <bill.noble@networkedinsights.com>
2017-11-10 06:25:20 -0600

    RNN LM progress

--- code/rnn_lm.py
+++ code/rnn_lm.py
@@ -1,8 +1,12 @@
 import torch.nn as nn
+from collections import Counter 
+import wiki
 
 class LSTM(nn.Module):
 
-    def __init__(self, max_tokens, embedding_dim, hidden_dim, num_layers, dropout):
+    """ https://github.com/pytorch/examples/tree/master/word_language_model """
+
+    def __init__(self, num_tokens, embedding_dim, hidden_dim, num_layers, dropout):
 
         super(LSTM, self).__init__()
 
@@ -11,9 +15,15 @@ class LSTM(nn.Module):
 
         # Layers
         self.dropout = nn.Dropout(dropout) # dropout for first & final layer 
-        self.embedding = nn.Embeding(max_num_tokens, embedding_dim)
+        self.embedding = nn.Embeding(num_tokens, embedding_dim)
         self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers, dropout=dropout)
-        self.final = nn.Linear(hidden_dim, max_tokens)
+        self.final = nn.Linear(hidden_dim, num_tokens)
+
+    def init_weights(self):
+        initrange = 0.1
+        self.embedding.weight.data.uniform_(-initrange, initrange)
+        self.final.bias.data.fill_(0)
+        self.final.weights.data.uniform_(-initrange, initrange)
 
     def forward(self, input_text, hidden):
         emb = self.dropout(self.embedding(input_text))
@@ -26,3 +36,55 @@ class LSTM(nn.Module):
         weight = next(self.parameters()).data
         return (Variable(weight.new(self.num_layers, batch_size, self.hidden_dim).zero()),
                 Variable(weight.new(self.num_layers, batch_size, self.hidden_dim).zero()))
+
+
+class Lexicon:
+
+    def __init__(self, tokens):
+        self._tokens = tokens
+        self._index_map = {t: i for i, t in enumerate(self._tokens)}
+
+    @classmethod
+    def fit(cls, texts, max_tokens):
+            for token in text:
+                token_count[token] += 1
+        return cls(max_tokens, token_count.most_common(max_tokens))
+
+    def token_to_index(t):
+        return self._index_map[t]
+
+    def index_to_token(i):
+        return self._tokens[i]
+
+    def encode_text(self, text):
+        tensor = torch.zeros(len(text)).long()
+        for i, t in enumerate(text):
+            tensor[i] = self.token_to_index(t)
+        if use_cuda:
+            var = Variable(tensor.cuda())
+        else:
+            var = Varibale(tensor)
+        return var
+
+    def __len__(self):
+        return len(self._tokens)
+
+
+if __name__ == '__main__':
+
+    max_tokens = 10000
+    embedding_dim = 200 
+    hidden_dim = 200 
+    num_layers = 2 
+    dropout = 0.5
+    learning_rate = 0.005
+    batch_size = 128
+
+    corpus = wiki.WikiCorpus.from_corpus_files()
+    posts = (post.clean_text for post in corpus.posts.values())
+    lexicon = Lexicon.fit(posts, max_tokens)
+
+
+     
+
+
